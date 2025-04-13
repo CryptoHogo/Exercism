300228c5aeb77f85fb9a176479903de80cb0edd8
CryptoHogo <aehogo@gmail.com>
2025-04-13 11:22:42 +0900

    Create raindrops.h
    Header file for raindrops.cpp

---
+++ raindrops.h
@@ -0,0 +1,12 @@
+#if !defined(RAINDROPS_H)
+#define RAINDROPS_H
+#include <string>
+
+namespace raindrops {
+    using namespace std;
+    
+    string convert(int input);
+
+}  // namespace raindrops
+
+#endif  // RAINDROPS_H
