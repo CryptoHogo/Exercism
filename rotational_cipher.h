4e6d5afb5292a5b432ad3a84c05c95ad7de9a077
CryptoHogo <aehogo@gmail.com>
2025-04-13 11:25:25 +0900

    Create rotational_cipher.h
    Header file for rotation_cipher.cpp

---
+++ rotational_cipher.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+namespace rotational_cipher {
+    std::string rotate(std::string, int);
+
+// TODO: add your solution here
+
+}  // namespace rotational_cipher
